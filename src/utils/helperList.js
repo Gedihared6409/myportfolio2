2493194fae35f29f8df13af51a8412f4abc7a767
ali <gediali019@gmail.com>
2021-09-23 15:53:41 +0300

    Deployment

---
+++ src/utils/helperList.js
@@ -0,0 +1,98 @@
+export const showcaseSkills = [{
+  label: 'Python',
+  rating: 80
+}, {
+  label: 'Django',
+  rating: 80
+},{
+label: 'PostgreSQL db',
+  rating: 50
+},
+{
+  label: 'Flask',
+  rating: 50
+}, {
+  label: 'React',
+  rating: 60
+}, {
+  label: 'Javascript',
+  rating: 50
+}, 
+];
+
+// src: 'https://reacttraining.com/images/react-router.svg'
+
+
+export const skillsList = [{
+//   label: 'HTML5',
+//   src: 'https://seeklogo.com/images/H/html5-logo-EF92D240D7-seeklogo.com.png'
+// }, {
+//   label: 'CSS3',
+//   src: 'https://seeklogo.com/images/C/css3-logo-8724075274-seeklogo.com.png'
+// }, {
+  label: 'Javascript',
+  src: 'https://img.icons8.com/color/144/000000/javascript.png',
+}, {
+  label: 'Django',
+  src: 'https://bit.ly/3kxYWTu',
+}, 
+{
+  label: 'Python',
+  src: 'https://bit.ly/3t5A4q0',
+  
+},
+{
+  label: 'Flask',
+  src: 'https://bit.ly/3yuAEhQ',
+},
+{
+  label: 'Heroku',
+  src: 'https://bit.ly/3gHfxCU',
+}, 
+{
+  label: 'React',
+  src: 'https://img.icons8.com/color/144/000000/react-native.png',
+}, {
+  label: 'PostgreSQL db',
+  src: 'https://bit.ly/3BqvjKt'
+}, {
+  label: 'Github',
+  src: 'https://img.icons8.com/fluent/144/000000/github.png'
+},  {
+  label: 'Jquery',
+  src: 'https://img.icons8.com/ios-filled/144/000000/jquery.png'
+},{
+  label: 'NPM',
+  src: 'https://img.icons8.com/color/144/000000/npm.png'
+}, 
+ {
+  label: 'Bootstrap',
+  src: 'https://i.ibb.co/zx0Tt9M/bootstrap.png'
+}, ];
+
+export const projectsList = [{
+  label: 'Asset Inventory- Python & Django',
+  imgSrc: '/images/Screenshot from 2021-08-30 12-12-23.png',
+  src: 'https://assets-inventoryy.herokuapp.com/',
+  githubSrc: 'https://github.com/Gedihared6409/asset-inventory',
+ 
+  description: 'This is an asset inventory system that enables an organization to keep track of all the assets and their state, to ensure easy identification of any asset which need repair or purchase.'
+  
+}, {
+  label: 'Ecomerce Website- Javascript , Python & Django',
+  imgSrc: '/images/Screenshot from 2021-08-30 12-13-06.png',
+  src: 'https://ecomerceweb.herokuapp.com/',
+  githubSrc: 'https://github.com/Gedihared6409/ecomerce',
+  description: 'This full ecomerce website with payment integration and users can able to shop as a registered user and as a geust without acctually having to create an accout and also checkout and never have to register'
+}, {
+  label: 'News webApp- Flask & python ',
+  src: 'https://listnews.herokuapp.com/',
+  imgSrc: "/images/ali.png",
+  
+}, {
+  label: 'Projects Awwards - Python & Django',
+  imgSrc: '/images/Screenshot from 2021-08-30 12-12-49.png',
+  src: 'https://awwards0.herokuapp.com/',
+  githubSrc: 'https://github.com/Gedihared6409/project-awad',
+  description: 'This project allows users to post their projects for other users to rate according to design, usability and content'
+}]
