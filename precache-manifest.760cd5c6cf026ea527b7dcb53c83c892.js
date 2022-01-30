3c697d46d00b10afd21b7862817d49b0adece997
ali <gediali019@gmail.com>
2022-01-30 12:43:46 +0300

    Updates

---
+++ precache-manifest.760cd5c6cf026ea527b7dcb53c83c892.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0055d2c34b9fd1a68f64aeef0fc5492f",
+    "url": "/myportfolio2/index.html"
+  },
+  {
+    "revision": "f141ed3e3f42fede5011",
+    "url": "/myportfolio2/static/css/2.02ecd3ae.chunk.css"
+  },
+  {
+    "revision": "3569b09ca3b680264607",
+    "url": "/myportfolio2/static/css/main.f337b96e.chunk.css"
+  },
+  {
+    "revision": "f141ed3e3f42fede5011",
+    "url": "/myportfolio2/static/js/2.41dd623e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myportfolio2/static/js/2.41dd623e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3569b09ca3b680264607",
+    "url": "/myportfolio2/static/js/main.7ec5c361.chunk.js"
+  },
+  {
+    "revision": "68bc3769816c109e1b94",
+    "url": "/myportfolio2/static/js/runtime-main.b327dfc8.js"
+  },
+  {
+    "revision": "d22363c27eebaf507055e262e51012c0",
+    "url": "/myportfolio2/static/media/curved.d22363c2.svg"
+  }
+]);
